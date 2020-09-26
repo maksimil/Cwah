e105547a3c6f0add27384d772ecb96eb4f465337
Maxim <maxim.kosterov@gmail.com>
2020-09-26 20:57:44 +0300

    Created boilerplate for login in game

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login: React.FC<{}> = () => {
-  return <div>Login</div>;
+  const [name, setname] = useState("");
+  const [key, setkey] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          value={name}
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          value={key}
+          onChange={(e) => {
+            setkey(e.target.value);
+          }}
+        />
+      </div>
+      <button
+        onClick={() => {
+          // login
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => {
+          // create room
+        }}
+      >
+        Create room
+      </button>
+    </div>
+  );
 };
 
 export default Login;
